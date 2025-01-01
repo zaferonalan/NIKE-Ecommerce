221a17ad249b6d468cc66d8cc7d5d544245292a7
zafergunayonalan <zafergunay7@gmail.com>
2025-01-01 18:01:45 +0300

    Pages-created

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,49 @@
+import Logo from '../assets/logo2.png'
+
+export const NavbarMenu = [
+    {
+      id: 1,
+      title: "Home",
+      link: "/",
+    },
+    {
+      id: 2,
+      title: "Mens",
+      link: "/mens",
+    },
+    {
+      id: 3,
+      title: "Womens",
+      link: "/womens",
+    },
+    {
+      id: 4,
+      title: "Kids",
+      link: "/kids",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      link: "/contact",
+    },
+  ];
 
 const Navbar = () => {
+
   return (
-    <div>Navbar</div>
-  )
+    <div className="text-white py-8">
+        <div className="container flex justify-between">
+            {/* logo */}
+            <div>
+                <img src={Logo} alt="" className='max-w-[100px]'/>
+            </div>
+            {/* Menu Section */}
+            <div className="">
+                
+            </div>
+        </div>
+    </div>
+)
 }
 
 export default Navbar
---
+++ src/Pages/Womens.jsx
@@ -0,0 +1,8 @@
+
+const Womens = () => {
+  return (
+    <div>Womens</div>
+  )
+}
+
+export default Womens
